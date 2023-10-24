757f2c7e0a338dd9e5acb399c30c9c5bef791059
andrekuru <andrewilliam13@gmail.com>
2023-10-24 13:41:28 -0300

    consumer cli added, but not working with generic mq client

--- src/bin/consumer.rs
+++ src/bin/consumer.rs
@@ -1,15 +1,40 @@
+use std::{
+    io::stdin,
+    net::ToSocketAddrs,
+};
+
 use anyhow::Result;
 use distribuida::{message_queue, Message, Tag};
+
+fn ask_prime(mq: message_queue::Client) -> Result<()> {
+    let request = Message::Request { prime_size: 42 };
+    println!("sending: {:?}", &request);
+    mq.send(request)?;
+    Ok(())
+}
+
+fn get_prime(mq: message_queue::Client) -> Result<()> {
+    let response = mq.receive::<Message>(Tag::Response)?;
+    println!("received: {:?}", &response);
+    Ok(())
+}
 
 fn main() -> Result<()> {
     let mq = message_queue::Client::new("127.0.0.1:8979");
 
-    let request = Message::Request { prime_size: 42 };
-    println!("sending: {:?}", &request);
-    mq.send(request)?;
+    println!("***Prime numbers client***");
+    loop {
+        let mut input = String::new();
+        stdin().read_line(&mut input);
+        
+        match input.trim() {
+            "ask" => ask_prime(&mq),
+            "get" => get_prime(&mq),
+            "exit" => break,
+            _ => (),
+        }
+    }
 
-    let response = mq.receive::<Message>(Tag::Response)?;
-    println!("received: {:?}", &response);
-
+    println!("***Client finished***");
     Ok(())
 }
