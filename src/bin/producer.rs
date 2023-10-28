0d6c3a25c7be8c2a294df80541c66fd7447349fa
Cauê Baasch de Souza <cauebs@pm.me>
2023-10-28 11:37:21 -0300

    Make producer multithreaded

--- src/bin/producer.rs
+++ src/bin/producer.rs
@@ -2,7 +2,7 @@ use ine5429_primes::functions;
 
 use distribuida::{message_queue, Message, Tag};
 
-use std::time::SystemTime;
+use std::{net::ToSocketAddrs, thread, time::SystemTime};
 
 fn generate_prime(prime_size: u32) -> Vec<u8> {
     let seed = SystemTime::now()
@@ -13,24 +13,22 @@ fn generate_prime(prime_size: u32) -> Vec<u8> {
     functions::find_fermat(u64::from(prime_size), &seed.into()).to_bytes_le()
 }
 
-fn main() {
-    env_logger::init();
-    log::info!("Producer initializing... ");
-
-    let server_addrs = std::env::args()
-        .skip(1)
-        .next()
-        .expect("Use: producer <mq_addr>:<mq_port>");
+fn run_producer<A: ToSocketAddrs>(server_addrs: A) {
+    let tid = thread::current().id();
 
     let mq = message_queue::Client::register(server_addrs)
-        .expect("Failed to register to message queue server");
+        .map_err(|e| log::error!("[{tid:?}] Failed to register to message queue server: {e:?}"))
+        .unwrap();
 
     loop {
-        let request = mq.receive::<Message>(Tag::Request).unwrap();
-        log::info!("Received message: {:?}", &request);
+        let request = mq
+            .receive::<Message>(Tag::Request)
+            .map_err(|e| log::error!("[{tid:?}] Failed to receive request: {e:?}"))
+            .unwrap();
+        log::info!("[{tid:?}] Received message: {:?}", &request);
 
         let Message::Request { prime_size } = request.inner else {
-            log::error!("Asked for a Request, but got a Response!");
+            log::error!("[{tid:?}] Asked for a Request, but got a Response!");
             continue;
         };
 
@@ -40,10 +38,29 @@ fn main() {
             recipient: request.sender,
             prime,
         };
-        log::info!("Sending message: {:?}", &response);
+        log::info!("[{tid:?}] Sending message: {:?}", &response);
         match mq.send(response) {
-            Ok(_) => log::info!("Response successfully sent"),
-            Err(e) => log::error!("Failed to respond request: {e:?}"),
+            Ok(_) => log::info!("[{tid:?}] Response successfully sent"),
+            Err(e) => log::error!("[{tid:?}] Failed to respond request: {e:?}"),
         }
     }
+}
+
+fn main() {
+    env_logger::init();
+    log::info!("Producer initializing...");
+
+    let mut args = std::env::args().skip(1);
+
+    let server_addrs = args
+        .next()
+        .expect("Use: producer <mq_addr>:<mq_port> [num_threads]");
+
+    let num_threads = args.next().and_then(|s| s.parse::<u32>().ok()).unwrap_or(1);
+    log::info!("Starting {num_threads} thread(s)");
+    thread::scope(|s| {
+        for _ in 0..num_threads {
+            s.spawn(|| run_producer(&server_addrs));
+        }
+    });
 }
